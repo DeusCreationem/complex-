3f947becf43fa1a113657bbc9a8a0d9cec7500fa
Artem <artemezh200627@gmail.com>
2025-08-05 19:04:38 +0300

    Fix Tailwind-PostCSS integration4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 //import { useState } from 'react'//
 import './App.css'
+import './index.css'
 import React from 'react';
 import Header from './components/layout/Header';
 import Hero from './components/sections/Hero';
